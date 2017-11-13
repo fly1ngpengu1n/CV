ac177573ed9170a78c644616418d6fc81807d2f9
fly1ngpengu1n <gabriel.boisvert.dev@gmail.com>
2017-11-13 05:55:10 +0100

    release

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+        <title>CV - GB</title>
+		<meta charset="utf-8">
+		<meta name="description" content="Ce document contient mon profils de carière et une liste de projet ajouté a mon portefolio."/>
+        <meta name="keywords" content="devellopeur, webMaster"/>
+        <meta name="author" content="Gabriel Boisvert"/>
+        <meta name="copyright" content="Copyright Gabriel Boisvert"/>
+        <meta name="robots" content="follow"/>
+        <link rel="stylesheet" href="CSS/styles.css"/>
+	</head>
+	<body>
+		<header>
+			<section>
+				<h1 class="titleFont">Gabriel Boisvert</h1>
+				<h3 class="titleFont">D&eacute;veloppeur logiciel</h3>
+				<p>
+					<a href="linkedin" href="https://www.linkedin.com/in/gabriel-boisvert-b24880130/">Linkedin</a>
+					&nbsp; -- gabriel.boisvert.dev@gmail.com -- &nbsp;
+					<a href="Documents/resume.pdf" download>Version PDF</a>
+				</p>
+			</section>
+		</header>
+		<section>
+			<h3 class="titleFont">Pr&eacute;sentation</h3>
+			<p>
+				&Eacute;tudiant de troisi&egrave;me ann&eacute;e en Informatique 
+				de gestion et de retour d’un stage int&eacute;gr&eacute; en France.
+				</br></br>
+				Je recherche un stage dans une entreprise professionnelle offrant 
+				un environnement de travail stimulant qui me permettra d’acqu&eacute;rir 
+				de l’exp&eacute;rience et d’approfondir mes connaissances techniques 
+				&agrave; savoir : l’analyse, la conception, la programmation et 
+				l’&eacute;laboration des jeux de tests.
+				</br></br>
+				Ma solide formation est un atout majeur afin que je puisse relever 
+				les d&eacute;fis du stage avec succès. 
+			</p>
+		</section>
+		<section>
+			<h3 class="titleFont">Exp&eacute;rience</h3>
+			<h3>D&eacute;veloppeur logiciel &agrave; Demand Side Instruments</h3>
+			<h4>Mai 2017 - Ao&ucirc;ts 2017 | Caen, France</h4>
+			<p>Installation de serveur web pour des appareils de communication</p>
+            <p>Conception d'un interface web pour la modification et maintenance des radios</p>
+        </section>
+		<section>
+			<h2>Comp&eacute;tences</h2>
+			<h4>Languages</h4>
+			<p>
+				★★★ C/C++, Java <br>
+				★★☆ SQL, PHP, Python, Bash <br>
+				★☆☆ Android, noSQL, JSP
+			</p>
+			<h4>Web</h4>
+			<p>PHP - Javascript - HTML5 - CSS3</p>
+			<h4>Base de donn&eacute;es</h4>
+			<p>Oracle - MySQL - MongoDB</p>
+			<h4>M&eacute;thodologies et framework</h4>
+			<p>
+				Hibernate 5 - UML - Agile/Scrum - Assurance qualit&eacute; - TDD <br>
+				Eclipse - Android studio - Git - Windows - Linux
+			</p>
+		</section>
+		<section>
+			<h2>&Eacute;ducation</h2>
+			<h4>Technique informatique, 420.AA</h4>
+            <h4>2015 - 2018 | C&eacute;gep Andr&eacute;-Laurendeau, LaSalle</h4>
+			<p>Une formation complète sur les différences tendance du marché actuel. </p>
+		</section>
+		<section>
+			<h2>Langues</h2>
+			<ul>
+				<li>Fran&ccedil;ais</li>
+				<li>Anglais</li>
+			</ul>
+			<h2>Sport</h2>
+			<ul>
+				<li>V&eacute;lo</li>
+				<li>Badminton</li>
+			</ul>
+		</section>
+		<section>
+			<a href="projet/">Voir les projets</a>
+		</section>
+	</body>
+</html>
